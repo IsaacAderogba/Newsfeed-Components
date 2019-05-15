f3547357a3605c4c9430350f62ee67d7ef59ece4
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-15 14:24:44 +0100

    Add code to slide menu back in

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,8 +3,7 @@ const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
   TweenMax.from('.menu', 1, {left: -200});
-  TweenMax.to('.menu', 1, {left: 0});
-
+  TweenMax.to('.menu', 1, {left: -50});
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -15,4 +14,20 @@ const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+menuButton.addEventListener('click', event => event.stopPropagation());
+menu.addEventListener('click', event => event.stopPropagation());
 
+
+
+// body click listener
+const body = document.querySelector('body');
+
+body.addEventListener('click', function() {
+  if(menu.classList.contains('menu--open')){
+    TweenMax.from('.menu', 1, {left: -50});
+    TweenMax.to('.menu', 1, {left: -350});
+    setTimeout(function () {
+      menu.classList.remove('menu--open');
+    }, 1000);
+  }
+});
