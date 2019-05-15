6b41c971625c209fec21d2bb24bad1f36f354434
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-15 14:33:58 +0100

    Refactor code for slide animations

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,9 +1,8 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
-  TweenMax.from('.menu', 1, {left: -200});
-  TweenMax.to('.menu', 1, {left: -50});
+
+  slideMenuOut();
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -22,12 +21,24 @@ menu.addEventListener('click', event => event.stopPropagation());
 // body click listener
 const body = document.querySelector('body');
 
-body.addEventListener('click', function() {
-  if(menu.classList.contains('menu--open')){
-    TweenMax.from('.menu', 1, {left: -50});
-    TweenMax.to('.menu', 1, {left: -350});
+body.addEventListener('click', slideMenuIn);
+
+function slideMenuIn() {
+  if (menu.classList.contains('menu--open')) {
+    TweenMax.from('.menu', 1, { left: -50 });
+    TweenMax.to('.menu', 1, { left: -350 });
     setTimeout(function () {
       menu.classList.remove('menu--open');
     }, 1000);
   }
-});
+}
+
+function slideMenuOut() {
+  if (menu.classList.contains('menu--open')) {
+    slideMenuIn();
+  } else {
+    menu.classList.toggle('menu--open');
+    TweenMax.from('.menu', 1, { left: -200 });
+    TweenMax.to('.menu', 1, { left: -50 });
+  }
+}
